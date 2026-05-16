d28c9eb1cd4c674643c30e9f848905dd22302723
Nishek Sharma <sharmanishek@yahoo.com>
2026-05-16 09:28:57 +0530

    Handles Request for withdraw and deposit operation and fetching balance amount in wallet

--- src/main/java/com/nsdev/walletapp/controller/WalletController.java
+++ src/main/java/com/nsdev/walletapp/controller/WalletController.java
@@ -1,25 +1,49 @@
 package com.nsdev.walletapp.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.nsdev.walletapp.dto.WalletRequest;
 import com.nsdev.walletapp.dto.WalletResponse;
+import com.nsdev.walletapp.service.WalletService;
 import com.nsdev.walletapp.util.ResponseStructure;
+import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
+@Validated
 public class WalletController {
-	
-	
+
+	@Autowired
+	private WalletService walletService;
+
+
 	@PostMapping("/wallet")
-	public ResponseEntity<ResponseStructure<WalletResponse>> processOperation(@RequestBody WalletRequest wr) {
-		
+	public ResponseEntity<ResponseStructure<WalletResponse>> handleWalletOperation(
+			@Valid @RequestBody WalletRequest request) {
+		WalletResponse response = walletService.processOperation(request);
+		ResponseStructure<WalletResponse> result = new ResponseStructure<>();
+		result.setSuccess(true);
+		result.setMessage("Operation completed successfully.");
+		result.setData(response);
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 
+	@GetMapping("/wallets/{walletId}")
+	public ResponseEntity<ResponseStructure<WalletResponse>> getWalletBalance(@PathVariable UUID walletId) {
+
+		WalletResponse response = walletService.getWalletBalance(walletId);
+
+		ResponseStructure<WalletResponse> result = new ResponseStructure<>();
+		result.setSuccess(true);
+		result.setMessage("Balance retrieved successfully.");
+		result.setData(response);
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
 }
