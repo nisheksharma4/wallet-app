220bda6b94d3ce8bc3e0816a59fcafb38a3b5c58
Nishek Sharma <sharmanishek@yahoo.com>
2026-05-16 09:22:30 +0530

    Repository File to perform operation on database

--- src/main/java/com/nsdev/walletapp/repository/WalletRepository.java
+++ src/main/java/com/nsdev/walletapp/repository/WalletRepository.java
@@ -1,5 +1,6 @@
 package com.nsdev.walletapp.repository;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nsdev.walletapp.entity.Wallet;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID>{
-
+	
+	int depositBalance(UUID walletId, BigDecimal amount);
+	
+	int withdrawBalance(UUID walletId, BigDecimal amount);
 }
