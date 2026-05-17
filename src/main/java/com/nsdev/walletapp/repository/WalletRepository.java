9a0d0ef0b0836bbb71f581125c7a34f56d6d3cbd
Nishek Sharma <sharmanishek@yahoo.com>
2026-05-17 14:20:28 +0530

    Added custom Query to handle concurrent requests

--- src/main/java/com/nsdev/walletapp/repository/WalletRepository.java
+++ src/main/java/com/nsdev/walletapp/repository/WalletRepository.java
@@ -1,15 +1,22 @@
 package com.nsdev.walletapp.repository;
 
+import com.nsdev.walletapp.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
-import com.nsdev.walletapp.entity.Wallet;
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Wallet w SET w.balance = w.balance + :amount WHERE w.id = :walletId")
+    int depositBalance(@Param("walletId") UUID walletId, @Param("amount") BigDecimal amount);
 
-public interface WalletRepository extends JpaRepository<Wallet, UUID>{
-	
-	int depositBalance(UUID walletId, BigDecimal amount);
-	
-	int withdrawBalance(UUID walletId, BigDecimal amount);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Wallet w SET w.balance = w.balance - :amount WHERE w.id = :walletId AND w.balance >= :amount")
+    int withdrawBalance(@Param("walletId") UUID walletId, @Param("amount") BigDecimal amount);
 }
